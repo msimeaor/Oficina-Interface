09db5f1c19a8215c389f72a397ac4097d14d498a
Matheus <maatsimeao@gmail.com>
2023-11-16 11:54:56 -0300

    Update App.jsx
    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react"
-import Header from './Components/Header/Header.jsx'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  const [currentPageName, setCurrentPageName] = React.useState('Bem Vindo!')
-
   return (
     <>
-      <BrowserRouter>
-        <Header currentPage={currentPageName} />
-        <Routes>
-          <Route path="/sistema-oficina/inicio" element={/*HomeScreen*/} />
-        </Routes>
-      </BrowserRouter>
     </>
   )
 }
