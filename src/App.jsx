cb2c05adc9e0c954ab7b756915cecc117f8463b4
Matheus <maatsimeao@gmail.com>
2023-11-16 11:18:07 -0300

    Creating the Header component
    - I created the Header component that will be displayed in all system screens. It contains a title indicating the current screen, the system name and a link to home screen.
    
    - On the App component, I created the route manager that define which screen will be displayed in the "/sistema-oficina/inicio" path. The link that leads of this path is within the Header
    - I also created a state that stores the current page name. The "setCurrentPageName" will be passed in every components changing this state to the name of the displayed screen.
    
    - I added some new dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react"
+import Header from './Components/Header/Header.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+  const [currentPageName, setCurrentPageName] = React.useState('Bem Vindo!')
+
   return (
     <>
+      <BrowserRouter>
+        <Header currentPage={currentPageName} />
+        <Routes>
+          <Route path="/sistema-oficina/inicio" element={/*HomeScreen*/} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
