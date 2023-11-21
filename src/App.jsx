b5e3506e6f237adc7cd7a252373d61a29526a220
Matheus <maatsimeao@gmail.com>
2023-11-21 16:16:16 -0300

    Creating Header component
    I created the Header component that will be diplayed on all screens, except for the login screen
    This component has a single static element representing the title of the screen. It changes as the page changes as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import Header from "./Components/Header/Header"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
-    <>
-    </>
+    <Header/>
   )
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link } from 'react-router-dom'
+
+const Header = ({ currentyPage }) => {
+  return (
+    <section className='container-fluid border-bottom' >
+      <section className='container pt-4 pb-4' >
+        <div className='row pb-2' >
+          <div className='col' >
+            <h2 className='text-left' >{currentyPage}</h2>
+          </div>
+          <div className='col' >
+            <h3 className='text-right' >Sistema Oficina</h3>
+          </div>
+        </div>
+        <div className='row pt-2' >
+          <div className='col' >
+            <h4><a href="#">Inicio</a></h4>
+          </div>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default Header
