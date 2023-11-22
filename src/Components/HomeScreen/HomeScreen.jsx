48f6009ae87dd86c8b5ff57e97f4dd2d63461b2d
Matheus <maatsimeao@gmail.com>
2023-11-22 14:44:22 -0300

    Updating HomeScreen component
    After creating the component that represents the HomeScreen links, I passed this component to represents each link of this screen. At the moment, we only have three links, but in the future we will have a few more.

--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import LinkToServiceScreen from './LinkToServiceScreen'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Person } from '../../Images/person-solid.svg'
+import { ReactComponent as House } from '../../Images/house-solid.svg'
+import { ReactComponent as Phone } from '../../Images/phone-solid.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const HomeScreen = ({ setCurrentPage }) => {
@@ -12,14 +15,9 @@ const HomeScreen = ({ setCurrentPage }) => {
   return (
     <section className='container' >
       <div className='row mt-5' >
-        <div className='col-lg-2 col-md-3 col-4 pt-2 pb-2' >
-          <Link className='d-flex flex-wrap justify-content-center' >
-            <Person/>
-            <h5 className='mt-2' >
-              Pessoas
-            </h5>
-          </Link>
-        </div>
+        <LinkToServiceScreen href='#' LinkIcon={<Person/>} linkDescription='Pessoas' />
+        <LinkToServiceScreen href='#' LinkIcon={<House/>} linkDescription='Endereços' />
+        <LinkToServiceScreen href='#' LinkIcon={<Phone/>} linkDescription='Telefones' />
       </div>
     </section>
   )
