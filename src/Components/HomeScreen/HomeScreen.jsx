e277ed06fe178e6a8bba628ef3cf1d193a5bc5d1
Matheus <maatsimeao@gmail.com>
2023-11-22 12:01:23 -0300

    Creating the fisrt link to API services
    I created this initial link which will be used later as a component to avoid code duplication.

--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Person } from '../../Images/person-solid.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const HomeScreen = ({ setCurrentPage }) => {
@@ -9,15 +11,14 @@ const HomeScreen = ({ setCurrentPage }) => {
 
   return (
     <section className='container' >
-      <div className='row' >
-        <div className='col-lg-2 col-md-3 col-4' >
-          
-        </div>
-        <div className='col-lg-2 col-md-3 col-4' >
-          
-        </div>
-        <div className='col-lg-2 col-md-3 col-4' >
-          
+      <div className='row mt-5' >
+        <div className='col-lg-2 col-md-3 col-4 pt-2 pb-2' >
+          <Link className='d-flex flex-wrap justify-content-center' >
+            <Person/>
+            <h5 className='mt-2' >
+              Pessoas
+            </h5>
+          </Link>
         </div>
       </div>
     </section>
