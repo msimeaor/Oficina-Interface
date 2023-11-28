619f752e26475972389b1385819a7f48e775502f
Matheus <maatsimeao@gmail.com>
2023-11-28 11:16:55 -0300

    Updating customRegistryScreen component
    After create the useForm hook, I replaced all states with the hooks.
    I created the inputsValues array that will recieve all the attributes of the form inputs. Therefore, I used the .map() method on this object and I rendered each input by passing its properties.

--- src/Components/CustomerScreen/CustomerRegistryScreen.jsx
+++ src/Components/CustomerScreen/CustomerRegistryScreen.jsx
@@ -2,98 +2,107 @@ import React from 'react'
 import Input from '../Form/Input/Input'
 import Select from '../Form/Select/Select'
 import Button from '../Form/Button/Button'
+import useForm from '../../Hooks/useForm'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const CustomerRegistryScreen = () => {
-  const [name, setName] = React.useState('')
-  const [cpf, setCpf] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [birthday, setBirthday] = React.useState('')
+  const name = useForm('name')
+  const cpf = useForm('cpf')
+  const email = useForm('email')
+  const address = useForm('address')
+  const birthday = useForm('birthday')
   const [gender, setGender] = React.useState('')
-  const [address, setAddress] = React.useState('')
 
   const genderOptions = ['Masculino', 'Feminino']
+
+  const inputsValues = [
+    {
+      type: 'Text',
+      label: 'Nome',
+      id: 'name',
+      placeholder: 'Insira o nome do cliente',
+      data: name
+    },
+    {
+      type: 'Text',
+      label: 'CPF',
+      id: 'cpf',
+      placeholder: 'Insira o CPF do cliente',
+      data: cpf
+    },
+    {
+      type: 'Text',
+      label: 'Email',
+      id: 'email',
+      placeholder: 'Insira o email do cliente',
+      data: email
+    },
+    {
+      type: 'Date',
+      label: 'Data de Nascimento',
+      id: 'birthday',
+      placeholder: '',
+      data: birthday
+    },
+    {
+      type: 'Text',
+      label: 'Endereço',
+      id: 'address',
+      placeholder: 'Insira o logradouro do cliente',
+      data: address
+    },
+  ]
 
   return (
     <section className='mt-5 mb-5' >
       <form>
         <div className='row' >
-          <div className='col-6' >
-            <Input
-              type='Text'
-              label='Nome'
-              id='name'
-              value={name}
-              handleChange={({target}) => setName(target.value)}
-              placeholder='Insira seu nome'
-            />
-          </div>
-          <div className='col-6' >
-            <Input
-                type='Text'
-                label='CPF'
-                id='cpf'
-                value={cpf}
-                handleChange={({target}) => setCpf(target.value)}
-                placeholder='Insira seu CPF'
-            />
-          </div>
-        </div>
-        <div className='row' >
-          <div className='col-6'>
-            <Input
-                type='Text'
-                label='Email'
-                id='email'
-                value={email}
-                handleChange={({target}) => setEmail(target.value)}
-                placeholder='Insira seu email'
-            />
-          </div>
-          <div className='col-6' >
-            <Input
-              type='Date'
-              label='Data de Nascimento'
-              id='birthday'
-              value={birthday}
-              handleChange={({target}) => setBirthday(target.value)}
-            />
-          </div>
-        </div>
-        <div className='row' >
+          {
+            inputsValues.map((inputValues) => (
+              <div key={inputValues.id} className='col-6' >
+                <Input
+                  id={inputValues.id}
+                  label={inputValues.label}
+                  type={inputValues.type}
+                  placeholder={inputValues.placeholder}
+                  {...inputValues.data}
+                />
+                {
+                  /*Create a function for button to find an address*/
+                  inputValues.id === 'address'
+                  ? (
+                    <Button
+                      description='Buscar Endereço'
+                    />
+                  ) : (
+                    null
+                  )
+                }
+              </div>
+            ))
+          }
           <div className='col-6' >
             <Select
-              id='sexo'
-              label='Sexo'
+              id='gender'
+              label='Gênero'
               options={genderOptions}
               value={gender}
               setValue={setGender}
             />
           </div>
-          <div className='col-6' >
-            <Input
-              type='Text'
-              label='Endereço'
-              id='address'
-              value={address}
-              handleChange={({target}) => setAddress(target.value)}
+        </div>
+        <div className='row mt-5' >
+          <div className='col-6 d-flex justify-content-end' >
+            {/*Create a function for button to clear the form*/}
+            <Button
+              description='Limpar Formulário'
             />
           </div>
-        </div>
-        <div className='row justify-content-end' >
-          <div className='col-4 d-flex justify-content-end' >
-            {/*Create the findAddress function*/}
-            <Button description='Buscar Endereço' />
-          </div>
-        </div>
-        <div className='row pt-5' >
-          <div className='col-6 d-flex justify-content-end' >
-            {/*Create the clearForm function*/}
-            <Button description='Limpar Formulário' />
-          </div>
           <div className='col-6' >
-            {/*Create the saveCustomer function*/}
-            <Button description='Salvar Cliente' />
+            {/*Create a function for button to save the customer*/}
+            <Button
+              description='Salvar Cliente'
+            />
           </div>
         </div>
       </form>
