f4475c8066249f8c1cef48030f1fb27e9269f8ce
Matheus <maatsimeao@gmail.com>
2023-12-05 09:57:08 -0300

    Updating the Header styles
    I added some Bootstrap styles and created a CSS file containing new styles.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from './Header.module.css'
 
 const Header = ({ currentPage }) => {
   return (
-    <section className='container-fluid border-bottom' >
+    <section className={`container-fluid border-bottom ${styles.containerFluid}`} >
       <section className='container pt-4 pb-4' >
         <div className='row pb-2' >
           <div className='col' >
-            <h2 className='text-left' >{currentPage}</h2>
+            <h2 className='text-left text-dark pt-2 pb-2' >{currentPage}</h2>
           </div>
           <div className='col' >
-            <h3 className='text-right' >Sistema Oficina</h3>
+            <h3 className='text-right text-dark pt-2 pb-2' >Sistema Oficina</h3>
           </div>
         </div>
         <div className='row pt-2' >
           <div className='col' >
-            <h4><Link to="/" >Inicio</Link></h4>
+            <Link to="/" className='btn btn-dark pl-4 pr-4' >Inicio</Link>
           </div>
         </div>
       </section>
