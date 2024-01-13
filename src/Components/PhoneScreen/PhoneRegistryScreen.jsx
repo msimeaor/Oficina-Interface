52ec26dfc5df4f35e1f4674ccc9cb0fe7e4a0266
Matheus <maatsimeao@gmail.com>
2024-01-13 19:37:45 -0300

    Creating the logic to search for and add an owner of the phone
    When saving a phone, we must say who owns this phone. In this code snippet, I created the logic for searching and adding this owner before saving the phone.

--- src/Components/PhoneScreen/PhoneRegistryScreen.jsx
+++ src/Components/PhoneScreen/PhoneRegistryScreen.jsx
@@ -1,23 +1,82 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Input from '../Form/Input/Input'
-import useForm from '../../Hooks/useForm'
 import Button from '../Form/Button/Button'
+import MyModal from '../Modal/MyModal'
+import endpointsApi from '../../json/EndpointsApi.json'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const PhoneRegistryScreen = () => {
   const number = useForm('phoneNumber')
   const owner = useForm('phoneOwner')
+  const [currentOwnerId, setCurrentOwnerId] = React.useState(null)
+  const [ownerId, setOwnerId] = React.useState(null)
+  const [modalData, setModalData] = React.useState({})
+  const {data, loading, error, doFetch} = useFetch()
 
-  function searchOwner() {
+  async function searchOwner() {
+    if (!ownerInputIsFilled()) {
+      showModal('Erro!', 'Digite o nome do proprietário para adicioná-lo')
+      return false
+    }
 
+    const ownerSearched = await fetchToSearchOwner()
+
+    if (ownerSearched !== false) {
+      fillOwnerInput(ownerSearched.nome)
+      setCurrentOwnerId(ownerSearched.id)
+    }
+  }
+
+  function ownerInputIsFilled() {
+    return owner.value.length > 0
+  }
+
+  function showModal(title, message) {
+    setModalData({title, message})
+  }
+
+  async function fetchToSearchOwner() {
+    const url = `${endpointsApi.defaultAddress}${endpointsApi.endpoints.person.findPersonByName}`.replace(':name', owner.value)
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const [response, json] = await doFetch(url, options)
+    
+    if (!response.ok) {
+      showModal('Erro!', json.mensagemErro)
+      return false
+    }
+
+    return json._embedded.pessoaResponseDTOList[0]
+  }
+
+  function fillOwnerInput(ownerName) {
+    owner.setValue(ownerName)
   }
 
   function addOwner() {
+    if (!currentOwnerIdExists()) {
+      showModal('Erro!', 'Busque um proprietário antes de adicioná-lo!')
+      return false
+    }
 
+    defineOwnerId()
   }
 
-  function removeOwner() {
+  function currentOwnerIdExists() {
+    return currentOwnerId !== null
+  }
 
+  function defineOwnerId() {
+    setOwnerId(currentOwnerId)
+    owner.setValue('')
+    setCurrentOwnerId(null)
   }
 
   return (
@@ -54,11 +113,9 @@ const PhoneRegistryScreen = () => {
             <div className='col-auto' >
               <Button className='btn btn-outline-secondary' description='Adicionar' handleClick={addOwner} />
             </div>
-            <div className='col-auto' >
-              <Button className='btn btn-outline-secondary' description='Remover' handleClick={removeOwner} />
-            </div>
           </div>
       </form>
+      {modalData && <MyModal {...modalData} setModalData={setModalData} />}
     </section>
   )
 }
