893270c41ae3656029ed986efb1eb9931be9c6f1
Matheus <maatsimeao@gmail.com>
2024-01-16 12:43:27 -0300

    Creating the function to search a phone by number
    I created the function that is called when the button to search for a phone is clicked.

--- src/Components/PhoneScreen/SearchPhoneScreen.jsx
+++ src/Components/PhoneScreen/SearchPhoneScreen.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
 import Input from '../Form/Input/Input'
 import Button from '../Form/Button/Button'
+import MyModal from '../Modal/MyModal'
 import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import endpointsApi from '../../json/EndpointsApi.json'
 import styles from './SearchPhoneScreen.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const SearchPhoneScreen = () => {
   const phoneNumber = useForm('phoneNumber')
+  const {data, loading, error, doFetch} = useFetch()
+  const [modalData, setModalData] = React.useState({})
 
   function searchForPhones() {
-    
+    if (!isInputFilled()) {
+      showModal('Erro!', 'Insira um número de telefone para buscá-lo')
+      return false
+    }
+
+    fetchForSearchPhone()
+  }
+
+  function isInputFilled() {
+    return phoneNumber.value.length > 0
+  }
+
+  function showModal(title, message) {
+    setModalData({title, message})
+  }
+
+  async function fetchForSearchPhone() {
+    const url = `${endpointsApi.defaultAddress}${endpointsApi.endpoints.phones.findPhoneByNumero}`.replace(':numero', phoneNumber.value)
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const [response, json] = await doFetch(url, options)
+
+    if (!response.ok)
+      showModal('Telefone não encontrado!', 'Não existem telefones cadastrados com este numero!')
+
+    // If the response.ok becomes true, the data will be filled and the table will be rendered
   }
 
   return (
@@ -39,6 +74,7 @@ const SearchPhoneScreen = () => {
         </div>
         <div className='col-lg-2' ></div>
       </div>
+      {modalData && <MyModal {...modalData} setModalData={setModalData} />}
     </section>
   )
 }
