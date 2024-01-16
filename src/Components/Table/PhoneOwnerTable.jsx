0cf2036e69446e13276c239737f8e57555ba5331
Matheus <maatsimeao@gmail.com>
2024-01-16 16:38:55 -0300

    Update PhoneOwnerTable.jsx

--- src/Components/Table/PhoneOwnerTable.jsx
+++ src/Components/Table/PhoneOwnerTable.jsx
@@ -1,34 +1,14 @@
 import React from 'react'
-import styles from './PhoneOwnerTable.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const PhoneOwnerTable = ({ownerData, ownerObjectAttributes, ownerAttributesDisplayed}) => {
-  function fillCell(value) {
-    if (Array.isArray(value)) {
-      return `${value[2]}/${value[1]}/${value[0]}`
-    }
-
-    return value
-  }
-
+const PhoneOwnerTable = ({ownerObject, ownerObjectAttributesDisplayed}) => {
   return (
     <div className='container' >
-      {
-        ownerObjectAttributes.map((attribute, index) => (
-          <div key={index} className={`row ${styles.borderedRow}`}  >
-            <div className={`${styles.ownerAttribute}`} >
-              {
-                ownerAttributesDisplayed[index]
-              }
-            </div>
-            <div className={`${styles.ownerData}`} >
-              {
-                fillCell(ownerData[attribute])
-              }
-            </div>
-          </div>
-        ))
-      }
+      {ownerObjectAttributesDisplayed.map((attribute, index) => (
+        <div key={index} >
+          
+        </div>
+      ))}
     </div>
   )
 }
